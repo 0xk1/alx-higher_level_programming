77a5b045d972517ca730cf9806dd4ec794dc54bc
0xk1 <ultra.saidi6@gmail.com>
2023-06-05 20:38:40 +0100

    1010

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,8 +11,10 @@ int check_cycle(listint_t *list)
 	listint_t *tortoise = list;
 	listint_t *hare = list;
 
+	if (!list)
+		return (0);
 
-	while (tortoise && hare)
+	while (tortoise && hare && hare->next)
 	{
 		tortoise = tortoise->next;
 		hare = hare->next->next;
