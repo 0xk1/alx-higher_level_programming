4f789e759bab9e96521338dc96502c452e6395dc
0xk1 <ultra.saidi6@gmail.com>
2023-06-06 10:52:04 +0100

    o_O

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,6 +3,6 @@ def fizzbuzz():
     for i in range(1, 101):
         if (i != 100):
             print("FizzBuzz" if not i % 3 and not i % 5 else "Fizz" if not i % 3
-                    else "Buzz" if not i % 5 else i, end=" ")
-		else:
-			 print("Buzz")
+                   else "Buzz" if not i % 5 else i, end=" ")
+        else:
+            print("Buzz")
