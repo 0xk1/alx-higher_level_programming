9ad72450ecdaf77b85f2b501756923ffb2c5a031
0xk1 <ultra.saidi6@gmail.com>
2023-06-11 12:48:53 +0100

    :)

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * comp_lists - comp lists
+ * @p1: p1
+ * @p2: p2
+ * Return: 1 if pal else 0
+ */
+int comp_lists(listint_t *p1, listint_t *p2)
+{
+	while (p1 && p2)
+	{
+		if (p1->n != p2->n)
+			return (0);
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	return (1);
+}
+/**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: head
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -8,44 +25,30 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow, *fast;
-	int *ints, i;
+	listint_t *slow, *fast, *prev, *next;
+	int is_pal;
 
 	slow = *head;
 	fast = *head;
+	prev = NULL;
+	next = NULL;
+
 	if (!*head || !(*head)->next)
 		return (1);
 
-	i = 0;
 	while (fast && fast->next)
 	{
-		slow = slow->next;
 		fast = fast->next->next;
-		i++;
+		next = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = next;
 	}
+
 	if (fast)
-		i++;
-	ints = malloc(sizeof(int) * i);
-	if (!ints)
-		return (0);
-	i = 0;
-	while (slow)
-	{
-		ints[i++] = slow->n;
 		slow = slow->next;
-	}
-	i--;
-	slow = *head;
-	while (slow && i >= 0)
-	{
-		if (slow->n != ints[i])
-		{
-			free(ints);
-			return (0);
-		}
-		i--;
-		slow = slow->next;
-	}
-	free(ints);
-	return (1);
+
+	is_pal = comp_lists(prev, slow);
+
+	return (is_pal);
 }
